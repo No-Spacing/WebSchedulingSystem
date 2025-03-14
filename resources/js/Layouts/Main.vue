01388455175d290faf0d287d0a478ddf6c90d5de
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-03-14 21:53:35 +0800

    create schedule link added

--- resources/js/Layouts/Main.vue
+++ resources/js/Layouts/Main.vue
@@ -22,11 +22,11 @@ const rail = ref(true)
             <v-list density="compact" nav>
                 <v-list-item href="#" prepend-icon="mdi-home" title="Home" value="home"></v-list-item>
                 <v-list-item href="/schedules" prepend-icon="mdi-calendar" title="Schedules" value="schedules"></v-list-item>
-                <v-list-item href="#" prepend-icon="mdi-calendar-edit" title="Create Schedule" value="createSchedule"></v-list-item>
+                <v-list-item href="/create-schedule" prepend-icon="mdi-calendar-edit" title="Create Schedule" value="createSchedule"></v-list-item>
             </v-list>
         </v-navigation-drawer>
 
-        <v-app-bar>
+        <v-app-bar elevation="3">
             <v-app-bar-nav-icon variant="text" @click.stop="rail = !rail"></v-app-bar-nav-icon>
             <v-app-bar-title>Scheduling Website</v-app-bar-title>
 

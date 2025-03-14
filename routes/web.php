572cf2bc9bea9855b719ba2b632a45729933fc33
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-03-14 21:54:38 +0800

    add post request url

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ScheduleController;
 
 Route::inertia('/schedules', 'Schedules');
+Route::inertia('/create-schedule', 'CreateSchedule');
+
+Route::post('/post-schedule', [ScheduleController::class, 'PostSchedule']);
+
+Route::post('/update-schedule', [ScheduleController::class, 'UpdateSchedule']);
 
 Route::get('/', function () {
     return view('welcome');

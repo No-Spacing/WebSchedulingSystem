3d3071dfedad1ae34622cf9fccd5d88e669d6001
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-03-14 18:30:12 +0800

    add modal dialog on update

--- resources/js/Pages/Schedules.vue
+++ resources/js/Pages/Schedules.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Layout from '../Layouts/Main.vue'
+import { ref } from 'vue'
 
 defineOptions({ layout: Layout })
-</script>
 
+const updateDialog = ref(false);
+</script>
 <template>
     <v-card title="Schedules" subtitle="List of schedules">
         <v-card-text>
@@ -30,13 +32,35 @@ defineOptions({ layout: Layout })
                         <td>14/03/2025</td>
                         <td>1:00AM - 2:00AM</td>
                         <td>
-                            <v-btn icon="mdi-pencil" class="ma-2 pa-2" size="x-small"></v-btn>
+                            <v-btn icon="mdi-pencil" @click="updateDialog = true" class="ma-2 pa-2" size="x-small"></v-btn>
                             <v-btn icon="mdi-trash-can-outline" class="ma-2 pa-2" size="x-small"></v-btn>
                         </td>
                     </tr>
                 </tbody>
             </v-table>
-
         </v-card-text>
     </v-card>
+    <v-dialog
+        v-model="updateDialog"
+        width="auto"
+    >
+        <v-card
+            title="Update Schedule"
+            subtitle="Edit schedule form"
+            width="450px"
+        >
+            <v-form>
+                <v-card-text>
+                    <v-text-field variant="outlined" label="Room No."></v-text-field>
+                    <v-text-field type="date" variant="outlined" label="Date"></v-text-field>
+                    <v-text-field type="time" variant="outlined" label="Start Time"></v-text-field>
+                    <v-text-field type="time" variant="outlined" label="End Time"></v-text-field>
+                    <v-card-actions class="d-flex justify-end">
+                        <v-btn @click="updateDialog = false">Close</v-btn>
+                        <v-btn>Submit</v-btn>
+                    </v-card-actions>
+                </v-card-text>
+            </v-form>
+        </v-card>
+    </v-dialog>
 </template>

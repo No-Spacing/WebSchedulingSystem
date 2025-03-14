00b2a989e97f99b3e56e2ac356e0f030ac8a7764
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-03-14 21:53:52 +0800

    added controller and post methods

---
+++ app/Http/Controllers/ScheduleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ScheduleRequest;
+use Inertia\Inertia;
+
+class ScheduleController extends Controller
+{
+    public function PostSchedule(ScheduleRequest $request){
+        
+    }
+
+    public function UpdateSchedule(ScheduleRequest $request){
+        
+    }
+}

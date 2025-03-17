1309ab66a8dde611773c8d4cd45addb9e361e370
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-03-17 21:02:07 +0800

    add CRUD functions

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -5,14 +5,50 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\ScheduleRequest;
 use Inertia\Inertia;
+use App\Models\Schedule;
+use Illuminate\Support\Facades\DB;
 
 class ScheduleController extends Controller
 {
+
     public function PostSchedule(ScheduleRequest $request){
-        
+        Schedule::create([
+            'title' => $request->title,
+            'room' => $request->room,
+            'date' => $request->date,
+            'startTime' => $request->startTime,
+            'endTime' => $request->endTime,
+        ]);  
     }
 
     public function UpdateSchedule(ScheduleRequest $request){
-        
+        DB::table('schedules')
+        ->where('id', $request->id)
+        ->update([
+            'title' => $request->title,
+            'room' => $request->room,
+            'date' => $request->date,
+            'startTime' => $request->startTime,
+            'endTime' => $request->endTime,
+        ]);
+    }
+
+    public function DeleteSchedule($id){
+        DB::table('schedules')
+        ->where('id', $id)
+        ->delete();
+    }
+
+    public function show(Request $request)
+    {
+        error_log($request->title);
+        $schedules = DB::table('schedules')
+        ->where('title', 'like', '%' . $request->search . '%')
+        ->orWhere('room', 'like', '%' . $request->search . '%')
+        ->orWhere('date', 'like', '%' . $request->search . '%')
+        ->paginate(5);
+        return Inertia::render('Schedules',[
+          'schedules' => $schedules
+        ]);
     }
 }

b6e47ad4c7f75eccad57aba1fa7275e9d2e893fa
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-03-17 21:02:23 +0800

    added CRUD Function

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\ScheduleController;
+use App\Models\Schedule;
 
-Route::inertia('/schedules', 'Schedules');
+Route::get('/schedules', [ScheduleController::class, 'show']);
+
 Route::inertia('/create-schedule', 'CreateSchedule');
 
 Route::post('/post-schedule', [ScheduleController::class, 'PostSchedule']);
 
 Route::post('/update-schedule', [ScheduleController::class, 'UpdateSchedule']);
+
+Route::delete('/delete-schedule/{id}', [ScheduleController::class, 'DeleteSchedule']);
 
 Route::get('/', function () {
     return view('welcome');
